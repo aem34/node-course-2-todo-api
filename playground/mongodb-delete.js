a0fafa84c489ac046f6dc9b0143c84cca92adde1
Renald Legendre <aem_info@yahoo.fr>
2019-01-20 00:58:38 +0100

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,39 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp')
+
+    // deleteMany
+/*    db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+        console.log(result);
+    });*/
+
+    // deleteOne
+/*    db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+        console.log(result);
+    })*/;
+
+    // findOneAndDelete
+/*    db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+        console.log(result);
+    });*/
+
+    // deleteMany
+/*    db.collection('Users').deleteMany({name: 'Andrew'}).then((result) => {
+        console.log(result);
+    });*/
+
+    // findOneAndDelete
+    db.collection('Users').findOneAndDelete({
+        _id: new ObjectID("5c393142e2178e1d9855481d")
+    }).then((result) => {
+        console.log(JSON.stringify(result, undefined, 2));
+    });
+
+ //   client.close();
+});
