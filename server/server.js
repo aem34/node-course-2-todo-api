418785787d55d3bdd7e7327c68b29982d9aab710
Renald Legendre <aem_info@yahoo.fr>
2019-02-10 00:43:27 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectId} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -26,6 +28,24 @@ app.get('/todos', (req, res) => {
         res.send({todos})
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectId.isValid(id)) {
+        res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            res.status(404).send();
+        }
+
+        res.send({todo: todo});
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
